2a61883fa29802c0f4b2c3662bdfb1a681fad3a7
Monika <moncobra@comcast.net>
2022-11-30 15:45:55 -0800

    created bunny

--- fetch-utils.js
+++ fetch-utils.js
@@ -26,6 +26,11 @@ export async function deleteBunny(id) {
 }
 
 export async function createBunny(bunny) {
+    const response = await client
+        .from('fuzzy_bunnies')
+        .insert({ bunny, user_id: client.auth.session().user.id });
+
+    return checkError(response);
     // create a bunny using the bunny argument
     // return checkError(response);
 }
