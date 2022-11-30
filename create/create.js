75169b45ec02520d5fe19267ed49f5e5e02f1171
Monika <moncobra@comcast.net>
2022-11-30 15:35:34 -0800

    completed load event listener for dropdown menu. families are now brought together(:

--- create/create.js
+++ create/create.js
@@ -15,8 +15,18 @@ form.addEventListener('submit', async (e) => {
 
 window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
+    const select = document.querySelector('select');
+    const families = await getFamilies();
     // grab the select HTML element from the DOM
     // go get the families from supabase
+    for (let family of families) {
+        const option = document.createElement('option');
+
+        option.value = family.id;
+        option.textContent = family.name;
+
+        select.append(option);
+    }
     // for each family
     // create an option tag
     // set the option's value and text content
